eb51d09a129b5ef57f59eda2725f7a8a485f2b24
madhu17982 <kesavanmadhusudhan@gmail.com>
2022-10-22 23:21:00 +0530

    Add files via upload

---
+++ racks/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import rack_Azure
+
+
+class djangoadmin(admin.ModelAdmin):
+    list_display=['subject','explanation']
+admin.site.register(rack_Azure,djangoadmin)
+
+
+
+
+
+
+
